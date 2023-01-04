e9e68ae1fdb8361c324bd5853c62089607c26985
Nicholas D. Socci <soccin@mskcc.org>
2023-01-03 22:28:35 -0500

    Work on loader

--- load_argos.R
+++ load_argos.R
@@ -1,3 +1,14 @@
 load_argos<-function(odir) {
 
+    pdir=file.path(odir,"portal")
+    dpt=read_tsv(file.path(pdir,"data_clinical_patient.txt"),comment="#")
+    sampleData=read_tsv(file.path(pdir,"data_clinical_sample.txt"),comment="#") %>%
+        left_join(dpt,by="PATIENT_ID")
+    maf=read_tsv(file.path(pdir,"data_mutations_extended.txt"),comment="#")
+    fusions=read_tsv(file.path(pdir,"data_fusions.txt"),comment="#")
+    cnv=read_tsv(file.path(pdir,"data_CNA.txt"),comment="#") %>%
+        gather(Sample,CNV,-Hugo_Symbol) %>% filter(CNV!=0 & !is.na(CNV))
+
+    list(sampleData=sampleData,maf=maf,cnv=cnv,fusions=fusions)
+
 }
