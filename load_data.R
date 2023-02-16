f2f6062c137cc852ddce1ffb72b3e8de76c01206
Nicholas D. Socci <soccin@mskcc.org>
2023-02-16 13:58:06 -0500

    Add summary and full cnv tables

--- load_data.R
+++ load_data.R
@@ -1,10 +1,20 @@
 source("load_argos.R")
 
+#################################################################
+
 argos_dir="data/argos/11704_Y/1.1.2/20221102_20_40_060423"
+
+#
+#
+#
+
 argos_data=load_argos(argos_dir)
 sampleID=names(argos_data)[1]
 
-geneAnnotation=load_gene_annotations()
+#################################################################
+#################################################################
+
+summaryTbl=get_summary_table(argos_data,sampleID)
 
 tbl01=get_clinical_table(argos_data,sampleID)
 
@@ -12,4 +22,8 @@ mafTbl=get_maf_table(argos_data,sampleID)
 
 cnvTbl=get_cnv_table(argos_data,sampleID)
 
+cnvTblFull=get_cnv_table_full(argos_data,sampleID)
+
+#################################################################
+#################################################################
 
