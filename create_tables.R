9627445a234641bf854cd49b91fcc71f66306277
Nicholas D. Socci <soccin@mskcc.org>
2023-02-16 12:45:25 -0500

    Refactor data loading and table creation

---
+++ create_tables.R
@@ -0,0 +1,34 @@
+get_clinical_table <- function(argosDb,sid) {
+
+    clinTbl=read_csv("data/section01.csv")
+    clinTbl$Value1=argosDb[[sid]][clinTbl$Value1] %>% unlist
+    clinTbl$Value2=argosDb[[sid]][clinTbl$Value2] %>% unlist
+    clinTbl$Value2['SAMPLE_ID']=gsub("s_","",clinTbl$Value2['SAMPLE_ID']) %>% gsub("_","-",.)
+
+    clinTbl
+
+}
+
+get_maf_table <- function(argosDb,sid) {
+    argosDb[[sid]]$MAF %>%
+        filter(!grepl("=$",HGVSp_Short)) %>%
+        mutate(`Additional Information`=paste0("MAF: ",round(100*t_var_freq,1),"%")) %>%
+        mutate(Alteration=gsub("^p.","",HGVSp_Short)) %>%
+        mutate(Alteration=paste0(Alteration," (",HGVSc,")")) %>%
+        mutate(Location=paste("exon",gsub("/.*","",EXON))) %>%
+        select(Gene=Hugo_Symbol,Type=Variant_Classification,Alteration,Location,`Additional Information`) %>%
+        mutate_all(~replace(.,grepl("^NA|NA$",.) | is.na(.),""))
+}
+
+get_cnv_table <- function(argosDb,sid) {
+    argosDb[[sid]]$CNV %>%
+        select(Gene=Hugo_Symbol,tcn,FACETS_CALL) %>%
+        filter(tcn>5 | tcn<1) %>%
+        left_join(geneAnnotation,by=c(Gene="hgnc.symbol")) %>%
+        filter(gene_biotype=="protein_coding") %>%
+        mutate(Type="Whole Gene",Alteration=FACETS_CALL) %>%
+        mutate(Location=paste0(chrom,band)) %>%
+        mutate(`Additional Information`=paste0("TCN: ",tcn)) %>%
+        arrange(chrom) %>%
+        select(Gene,Type,Alteration,Location,`Additional Information`)
+}
--- load_data.R
+++ load_data.R
@@ -1,4 +1,5 @@
 source("load_argos.R")
+source("create_tables.R")
 
 suppressPackageStartupMessages({
     library(tidyverse)
@@ -8,38 +9,18 @@ suppressPackageStartupMessages({
 
 if(interactive() && exists("SOURCED") && SOURCED) halt(".INCLUDE")
 
+
 argos_dir="data/argos/11704_Y/1.1.2/20221102_20_40_060423"
-aa=load_argos(argos_dir)
-samp=names(aa)[1]
+argos_data=load_argos(argos_dir)
+sampleID=names(argos_data)[1]
 
 geneAnnotation=readRDS("data/geneAnnotation.rds")
 
-tbl01=read_csv("data/section01.csv")
-tbl01$Value1=aa[[samp]][tbl01$Value1] %>% unlist
-tbl01$Value2=aa[[samp]][tbl01$Value2] %>% unlist
-tbl01$Value2['SAMPLE_ID']=gsub("s_","",tbl01$Value2['SAMPLE_ID']) %>% gsub("_","-",.)
+tbl01=get_clinical_table(argos_data,sampleID)
 
-#css.cell=c("padding-left: .25em","padding-left: 1em; padding-right: 6em;","padding-left: .25em;","padding-left: 1em;")
+mafTbl=get_maf_table(argos_data,sampleID)
 
-mafTbl=aa[[samp]]$MAF %>%
-    filter(!grepl("=$",HGVSp_Short)) %>%
-    mutate(`Additional Information`=paste0("MAF: ",round(100*t_var_freq,1),"%")) %>%
-    mutate(Alteration=gsub("^p.","",HGVSp_Short)) %>%
-    mutate(Alteration=paste0(Alteration," (",HGVSc,")")) %>%
-    mutate(Location=paste("exon",gsub("/.*","",EXON))) %>%
-    select(Gene=Hugo_Symbol,Type=Variant_Classification,Alteration,Location,`Additional Information`) %>%
-    mutate_all(~replace(.,grepl("^NA|NA$",.) | is.na(.),""))
-
-cnvTbl=aa[[samp]]$CNV %>%
-    select(Gene=Hugo_Symbol,tcn,FACETS_CALL) %>%
-    filter(tcn>5 | tcn<1) %>%
-    left_join(geneAnnotation,by=c(Gene="hgnc.symbol")) %>%
-    filter(gene_biotype=="protein_coding") %>%
-    mutate(Type="Whole Gene",Alteration=FACETS_CALL) %>%
-    mutate(Location=paste0(chrom,band)) %>%
-    mutate(`Additional Information`=paste0("TCN: ",tcn)) %>%
-    arrange(chrom) %>%
-    select(Gene,Type,Alteration,Location,`Additional Information`)
+cnvTbl=get_cnv_table(argos_data,sampleID)
 
 SOURCED=T
 
