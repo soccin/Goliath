967b4072408b25000acd48f37b5e4182148534e9
Nicholas D. Socci <soccin@mskcc.org>
2023-03-08 18:48:07 -0500

    Added fusions and summary tables

--- load_data.R
+++ load_data.R
@@ -1,12 +1,11 @@
 source("load_argos.R")
+require(glue)
 
 #################################################################
 
 load_data<-function(argos_dir,sampleID) {
 
     argos_data=load_argos(argos_dir)
-
-    summaryTbl=get_summary_table(argos_data,sampleID)
 
     tbl01=get_clinical_table(argos_data,sampleID)
 
@@ -16,12 +15,30 @@ load_data<-function(argos_dir,sampleID) {
 
     cnvTblFull=get_cnv_table_full(argos_data,sampleID)
 
+    fusionTbl=get_fusion_table(argos_data,sampleID)
+
+    nMut=nrow(mafTbl)
+    nCNV=nrow(cnvTblFull)
+    nFusion=nrow(fusionTbl)
+
+    summaryTxt=glue("{nMut} mutations, {nCNV} copy number alterations, {nFusion} structural variant dectected")
+    msiTxt=glue("MSI Status = {MSI_STATUS}, score = {MSI_SCORE}",.envir=argos_data[[sampleID]])
+    tmbTxt=glue("The estimated tumor mutation burden (TMB) for this sample is {CMO_TMB_SCORE} mutations per megabase (mt/Mb).",.envir=argos_data[[sampleID]])
+
+    summaryTbl=tribble(
+        ~Section, ~Data,
+        "Summary:", summaryTxt,
+        "MSI Status:", msiTxt,
+        "Tumor Mutations Burden:", tmbTxt
+    )
+
     list(
         summaryTbl=summaryTbl,
         tbl01=tbl01,
         mafTbl=mafTbl,
         cnvTbl=cnvTbl,
-        cnvTblFull=cnvTblFull
+        cnvTblFull=cnvTblFull,
+        fusionTbl=fusionTbl
     )
 
 }
