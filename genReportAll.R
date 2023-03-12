9546da1ef25acce1ac9dd9fd69987c6d6a57badf
Nicholas D. Socci <soccin@mskcc.org>
2023-03-11 21:33:52 -0500

    Report on all samples

---
+++ genReportAll.R
@@ -0,0 +1,31 @@
+args=commandArgs(trailing=T)
+if(length(args)!=2) {
+    cat("\n   usage: genReport.R REPORT.Rmd argosFolder\n\n")
+    quit()
+}
+
+
+source("load_argos.R")
+argos=load_argos(args[2])
+
+
+VERSION="0.2.1"
+projectNo=stringi::stri_match(args[2],regex="argos/([^/]+)/")[2]
+
+
+for(si in unique(names(argos))) {
+
+    params=list(
+        argosDir=args[2],
+        sampleID=si
+    )
+
+    rmarkdown::render(
+        args[1],
+        params=params,
+        output_format="html_document",
+        output_file=paste0("rpt_",projectNo,"-",params$sampleID,"__",VERSION,".html"),
+        clean=T
+    )
+
+}
