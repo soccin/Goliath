f2efd9adf5dd4e2f4575097a80fcc692458c089c
Nicholas D. Socci <soccin@mskcc.org>
2023-03-30 13:30:11 -0400

    Check for bad sample ID

--- load_data.R
+++ load_data.R
@@ -11,6 +11,12 @@ load_data<-function(argos_dir,sampleID) {
 
     argos_data=load_argos(argos_dir)
 
+    if(is.null(argos_data[[sampleID]])) {
+        cat("\n\nFATAL ERROR: invalid sample",sampleID,"\n")
+        cat("argos_dir =",argos_dir,"\n\n\n")
+        rlang::abort("FATAL ERROR")
+    }
+
     isUnMatched=argos_data[[sampleID]]$MATCH == "UnMatched"
 
     tbl01=get_clinical_table(argos_data,sampleID)
@@ -57,7 +63,7 @@ load_data<-function(argos_dir,sampleID) {
 
     reportTbl=tribble(
         ~key,~value,
-        "Report:","Argos Report (version 0.9.5)",
+        "Report:","Argos Report (version 0.9.6)",
         "Run Folder:", runFolder,
         "Data UUID:", digest::digest(argos_data[[sampleID]])
         )
@@ -67,6 +73,7 @@ load_data<-function(argos_dir,sampleID) {
         summaryTbl=summaryTbl,
         tbl01=tbl01,
         mafTbl=mafTbl,
+        mafTblFull=mafTblFull,
         cnvTbl=cnvTbl,
         cnvTblFull=cnvTblFull,
         fusionTbl=fusionTbl,
