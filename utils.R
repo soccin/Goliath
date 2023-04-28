131c822ed70a9527b237035a9eab0b8e14b98c51
Nicholas D. Socci <soccin@mskcc.org>
2023-04-28 17:55:14 -0400

    Add utils functions

---
+++ utils.R
@@ -0,0 +1,24 @@
+load_oncokb <- function() {
+
+    ARGOS_ANNOTATION_RESOURCES="/juno/work/ci/resources/genomic_resources/annotations/"
+    oo=readRDS(last(fs::dir_ls(file.path(ARGOS_ANNOTATION_RESOURCES,"oncokb"))))
+
+    #
+    # Need this for join, Our MAF's keep Position fields as numbers
+    #
+    oo$dat=oo$dat %>% mutate(Start_Position=as.numeric(Start_Position),End_Position=as.numeric(End_Position))
+    oo
+
+}
+
+load_gene_annotations <- function() {
+    readRDS("data/geneAnnotation.rds")
+}
+
+load_methods <- function() {
+    readLines("data/methods.md")
+}
+
+load_glossary <- function() {
+    read_csv("data/glossary.csv")
+}
