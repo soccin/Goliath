f51e26eff3b664f7e266fd7a696dad09f878fbed
Nicholas D. Socci <soccin@mskcc.org>
2023-05-12 23:14:06 -0400

    Fixed other report scripts

--- bin/genReport.R
+++ bin/genReport.R
@@ -1,12 +1,24 @@
+get_input_dirs<-function(root_dir) {
+
+    analysis_dir=fs::dir_ls(root_dir) %>% grep("analysis$",.,value=T) %>% unname()
+    portal_dir=fs::dir_ls(root_dir) %>% grep("portal$",.,value=T) %>% unname()
+
+    list(
+        analysis_dir=analysis_dir,
+        portal_dir=portal_dir
+    )
+
+}
+
 args=commandArgs(trailing=T)
 if(length(args)!=3) {
-    cat("\n   usage: genReport.R REPORT.Rmd argosFolder sampleID\n\n")
+    cat("\n   usage: genReport.R REPORT.Rmd argosFolder sample_id\n\n")
     quit()
 }
 
 params=list(
-    argosDir=args[2],
-    sampleID=args[3]
+    inputs=get_input_dirs(args[2]),
+    sample_id=args[3]
 )
 
 VERSION="0.2.1"
@@ -17,7 +29,7 @@ rmarkdown::render(
     args[1],
     params=params,
     output_format="html_document",
-    output_file=paste0("rpt_",projectNo,"-",params$sampleID,"__",VERSION,".html"),
+    output_file=paste0("rpt_",projectNo,"-",params$sample_id,"__",VERSION,".html"),
     intermediates_dir=tempdir(),
     clean=T
 )
